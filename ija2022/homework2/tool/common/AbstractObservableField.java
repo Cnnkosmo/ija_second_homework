850a66272c7aab0b100abb12dd5c6493bf3ef05d
Cnnkosmo <hristoigor920@gmail.com>
2023-04-03 13:59:10 +0200

    skelet of program

---
+++ ija2022/homework2/tool/common/AbstractObservableField.java
@@ -0,0 +1,4 @@
+package ija.ija2022.homework2.tool.common;
+
+public class AbstractObservableField {
+}
---
+++ ija2022/homework2/tool/view/ComponentView.java
@@ -0,0 +1,4 @@
+package ija.ija2022.homework2.tool.view;
+
+public interface ComponentView {
+}
